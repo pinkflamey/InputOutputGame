09e39038f23b9a8a5e3848c63cadb4182fb0a77d
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2023-02-13 12:11:38 +0100

    imports

---
+++ InAndOut/Assets/Code/Player/PlayerMovement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotationDuration = 3f;
+
+
+    public bool movementLocked = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Movement checks
+        if (!movementLocked) //Only allow movement input while the movement IS NOT locked
+        {
+            
+            //Walk forward
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+
+            //Rotate left (-90 degrees)
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Rotate(-90);
+            }
+        
+            //Rotate right (+90 degrees)
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Rotate(90);
+            }
+            
+        }
+
+    }
+
+    void Rotate(float degrees)
+    {
+        Hashtable hash = iTween.Hash
+            (
+            "amount", new Vector3(0, degrees, 0), //Amount to rotate
+            "time", rotationDuration, //Time for the rotation to take
+            "onstarttarget", gameObject,
+            "onstart", "SetMovementState", //Lock movement
+            "oncompletetarget", gameObject,
+            "oncomplete", "SetMovementState", //Unlock movement
+            "easetype", iTween.EaseType.linear //Rotation is linear
+            );
+
+        iTween.RotateAdd(gameObject, hash);
+
+    }
+
+    void SetMovementState()
+    {
+        Debug.Log("function running");
+
+        if (!movementLocked) //If the movement IS NOT locked
+        {
+            movementLocked = true; //Lock the movement
+        }
+        else if (movementLocked) //If the movement IS locked
+        {
+            movementLocked = false; //Unlock the movement
+        }
+    }
+}
