b024ec8dc94d5fe1810c04395898d3c1d22a1574
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2023-02-19 12:35:10 +0100

    Added door locking mechanism
    And changed prefab file tree

--- InAndOut/Assets/Code/Environment/Door.cs
+++ InAndOut/Assets/Code/Environment/Door.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private bool trigger = false;
+    [SerializeField] private bool triggerLock = false;
 
     public bool isOpen = false;
+    public bool isLocked = false;
 
     private Animator animator;
     
@@ -19,27 +21,40 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (trigger)
+        if (trigger) //When triggered;
         {
-            trigger = !trigger;
+            trigger = !trigger; //Reset the trigger
 
-            if (isOpen)
+            if (isOpen) //If the door is open;
             {
-                //Close
-                animator.SetTrigger("Close");
-                isOpen = !isOpen;
+                animator.SetTrigger("Close"); //Close it
+                isOpen = !isOpen; //Set isOpen to false (door is closed)
             }
-            else
+            else if(!isOpen && !isLocked) //If the door is closed and not locked;
             {
-                //Open
-                animator.SetTrigger("Open");
-                isOpen = !isOpen;
+                animator.SetTrigger("Open"); //Open the door
+                isOpen = !isOpen; //Set isOpen to true (door is open)
             }
+            else if (!isOpen && isLocked) //If the door is closed but locked;
+            {
+                Debug.Log("Door is locked!"); //Door is locked
+            }
+        }
+
+        if (triggerLock)
+        {
+            triggerLock = !triggerLock;
+            isLocked = !isLocked;
         }
     }
 
     public void TriggerDoor()
     {
         trigger = true;
+    }
+
+    public void TriggerDoorLock()
+    {
+        triggerLock = true;
     }
 }
