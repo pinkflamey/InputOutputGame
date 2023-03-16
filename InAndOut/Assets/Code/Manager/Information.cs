24d46f98eafa1338d2d50fe5fdee48900d5a389c
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2023-03-16 11:59:16 +0100

    removed ardity, started v0.1 build

--- InAndOut/Assets/Code/Manager/Information.cs
+++ InAndOut/Assets/Code/Manager/Information.cs
@@ -1,18 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Shapes2D;
+using TMPro;
 using UnityEngine;
 
 public class Information : MonoBehaviour
 {
-    public int heartrate;
+    [Header("Information")]
+    [SerializeField] private int heartrate;
 
+    //Private internal variables
     private ReadTextFile rtf;
+    private TextMeshProUGUI debug_hr_ui;
 
     // Start is called before the first frame update
     void Start()
     {
         rtf = GetComponent<ReadTextFile>();
+        debug_hr_ui = GameObject.Find("HR-debug").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -20,5 +26,13 @@ public class Information : MonoBehaviour
     {
         //Store heartrate in information script, not only in ReadTextFile
         Int32.TryParse(rtf.GetFileData(), out heartrate);
+        
+        //Set debug text
+        debug_hr_ui.text = heartrate.ToString();
+    }
+
+    public int GetHeartRate()
+    {
+        return heartrate;
     }
 }
