4221de7ff5d44a4471700b3497fefc421c65df88
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2023-04-06 09:22:27 +0200

    tested out Input System with switch pro controller

---
+++ InAndOut/Assets/InputManager/ControllerTester.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerTester : MonoBehaviour
+{
+    public void OnTurnLeft()
+    {
+        Debug.Log("Turning left!");
+    }
+}
