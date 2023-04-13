1572df2e48e8e2b934527ea8324cce88a95b1406
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2023-04-13 09:58:23 +0200

    Added room hierarchy and gizmos

---
+++ InAndOut/Assets/Code/RoomGizmo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RoomGizmo : MonoBehaviour
+{
+    public float lineThickness;
+    private void OnDrawGizmos()
+    {
+        foreach (Transform c in transform)
+        {
+            if (c.gameObject.tag == "RoomParent")
+            {
+                Handles.color = Color.red;
+                Handles.Label(c.position, c.gameObject.name);
+                
+            }
+            else
+            {
+                Gizmos.DrawCube(c.position, Vector3.one);
+            }
+        }
+    }
+}
